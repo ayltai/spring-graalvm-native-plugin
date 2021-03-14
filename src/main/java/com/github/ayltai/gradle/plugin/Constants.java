12dbf3ebeec00b30c73ab7af967aa8987adae8d9
Misagh Moayyed <mm1844@gmail.com>
2021-03-14 11:21:52 +0330

    Update Constants.java

--- src/main/java/com/github/ayltai/gradle/plugin/Constants.java
+++ src/main/java/com/github/ayltai/gradle/plugin/Constants.java
@@ -1,7 +1,7 @@
 package com.github.ayltai.gradle.plugin;
 
 public final class Constants {
-    public static final String SPRING_GRAALVM_VERSION = "0.8.5";
+    public static final String SPRING_GRAALVM_VERSION = "0.9.0";
     public static final String DEFAULT_TOOL_VERSION   = "21.0.0";
     public static final String DEFAULT_JAVA_VERSION   = "8";
     public static final String DOWNLOAD_ALWAYS        = "always";
