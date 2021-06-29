2bee8e1647cd327d36acf74d6635f4c1b7d1bf25
Misagh Moayyed <mm1844@gmail.com>
2021-06-29 19:04:06 +0400

    Upgrade Spring Native

--- src/main/java/com/github/ayltai/gradle/plugin/Constants.java
+++ src/main/java/com/github/ayltai/gradle/plugin/Constants.java
@@ -1,7 +1,7 @@
 package com.github.ayltai.gradle.plugin;
 
 public final class Constants {
-    public static final String SPRING_NATIVE_VERSION = "0.10.0";
+    public static final String SPRING_NATIVE_VERSION = "0.10.1";
     public static final String DEFAULT_TOOL_VERSION  = "21.1.0";
     public static final String DEFAULT_JAVA_VERSION  = "8";
     public static final String DOWNLOAD_ALWAYS       = "always";
